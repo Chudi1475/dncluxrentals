d9e9fa98ffa43ba42b09c2bfc774e936a97d2bac
Chudi1475 <chudid777@gmail.com>
2025-07-10 02:08:42 -0500

    here

--- dncluxrentals/settings.py
+++ dncluxrentals/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4u&g_ti_@v0f0-)&uorj*z9m(4d2$33g64gwr!&%o8)&+*qh8$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!.
 DEBUG = True
 
 ALLOWED_HOSTS = []
